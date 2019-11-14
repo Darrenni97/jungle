a3a8da0931c53b3723e7b814ba0064c3249e3ef4
Darrenni97 <darrenni_@hotmail.com>
2019-11-14 13:19:19 -0800

    implmented admin categories

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: %i[edit update show]
+    resources :categories, only: %i[create index new]
   end
 
   get 'about' => 'about#show'
